fc35bf781a9fc70baf765c6d0b12b8f0158e7a52
Yaiser <yaiserar@gmail.com>
2021-07-13 19:12:58 +0200

    timer finished

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
 /**Jose*/
 
-var mainWhenClicked = document.getElementById("mainButtonClick");
-
 /**Andres */
 
 /**Yaiser */
+
+var startBtn = document
+    .getElementById("main-button-click")
+    .addEventListener("click", timer);
+var cardClick = document
+    .getElementById("card4")
+    .addEventListener("click", () => {
+        winTime();
+        stopInterval();
+    });
+
+var counter = 0;
+var timing = 0;
+
+function timer() {
+    timing = setInterval(() => {
+        counter += 0.1;
+        console.log(counter);
+    }, 100);
+}
+
+function winTime() {
+    var ranking = [];
+    var timeMine = ranking.push(counter);
+    console.log(timeMine);
+    console.log(ranking);
+}
+
+function stopInterval() {
+    clearInterval(timing);
+}
