6b81173bdd4ea6e5820111d86e4efbd7f3bdbe5d
Jose1i1o <guadix88@gmail.com>
2021-07-13 19:14:21 +0200

    update Tuesday

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 /**Jose*/
 
-var mainWhenClicked = document.getElementById("mainButtonClick");
+/* DOM VARIABLES */
+
+// Pages
+let homePage = document.getElementById("article__page1");
+let gamePage = document.getElementById("article__page2");
+
+// Next buttons
+let btnNext = document
+    .getElementById("next")
+    .addEventListener("click", page1to2);
+
+function page1to2() {
+    homePage.style.display = "none";
+    gamePage.classList.remove("display-none");
+}
+
+function activateCounter() {}
 
 /**Andres */
 
