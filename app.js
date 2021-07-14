0ec7b13fa9821628e0c06245189cf3eb4dde177e
andarbech <andarbech1.com>
2021-07-14 21:25:09 +0200

    functional pack done

--- app.js
+++ app.js
@@ -1,77 +1,51 @@
 /**Jose*/
 /* DOM VARIABLES */
 
-// Pages
+// ----------------------------------------------Get elements--------------------------------------------
 let homePage = document.getElementById("article__page1");
 let gamePage = document.getElementById("article__page2");
-
-// Next buttons
-let btnNext = document.getElementById("next").addEventListener("click", () => {
-    page1to2();
-    timer();
-});
-
-function page1to2() {
-    homePage.style.display = "none";
-    gamePage.classList.remove("display-none");
-}
-
-function activateCounter() {}
-/**Andres */
-let $inputConfirm = document.querySelector("#input-confirm");
-$inputConfirm.addEventListener("click", getValue);
-
-function getValue() {
-    var $userName = document.querySelector("#input-username").value;
-    console.log($userName);
-    let player = $userName;
-    let playerArray = [];
-    for (let $usernames of player) {
-        playerArray.push($usernames);
-        let userNameComp = playerArray.join("");
-        $classification = document.querySelector("#player").innerHTML = userNameComp
-        console.log($classification)
-    }
-    /*for (let index = 0; index < $classification.length; index++) {
-        const element = array[index];
-        console.log(element)
-
-    }
-
-    //let playersss = playerArray.join("")
-    // console.log(typeof(playersss))*/
-}
-/* const ranking = {
-                length: 5,
-                player: $userName.textContent
-            } */
-/**Yaiser */
-
-// var startBtn = document
-//     .getElementById("main-button-click")
-//     .addEventListener("click", timer);
+var $userName = document.querySelector("#input-username").value // input value
+var $inputConfirm = document.querySelector("#input-confirm");
 var cardClick = document
-    .getElementById("card4")
+    .getElementById("card4") //event to capture click cards events and stop counter
     .addEventListener("click", () => {
         winTime();
         stopInterval();
     });
-
+//-----------------------------------------------------Variables--------------------------------------------
 var counter = 0;
 var timing = 0;
+//------------------------------------------------ Add Event Listener---------------------------------------
+let btnNext = document.getElementById("next").addEventListener("click", () => { //when you click start game we have two events
+    page1to2(); //switch pages
+    timer(); //Satr the counter
+});
+$inputConfirm.addEventListener("click", getValue);
+// --------------------------------------------------Functions----------------------------------------
+function getValue() { //store the name player in array
+    var playerArray = [];
+    var fullName = playerArray.push($userName);
+    console.log(playerArray);
+}
+
+function page1to2() {
+    homePage.style.display = "none"; //switch styles 
+    gamePage.classList.remove("display-none");
+}
 
 function timer() {
-    timing = setInterval(() => {
+    timing = setInterval(() => { //counter start from zero (0.1 sec)
         counter += 0.1;
         console.log(counter);
     }, 100);
 }
 
-function winTime() {
+function winTime() { //store the score in array
     var ranking = [];
     var timeMine = ranking.push(counter);
     console.log(timeMine);
     console.log(ranking);
+
 }
 
 function stopInterval() {
